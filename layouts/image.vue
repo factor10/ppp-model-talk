f6fd092f265e556a40a34bd6dfe6b04e1d95badf
Daniel Raniz Raneland <raniz@factor10.com>
2023-03-08 06:32:23 +0100

    Fix logo on the image layout

--- layouts/image.vue
+++ layouts/image.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { defineComponent, computed } from 'vue'
-import Default from '../layouts/default.vue'
+import Factor10Logo from '../components/Factor10Logo.vue'
 import { handleBackground } from '../layoutHelper'
 const props = defineProps({
   image: {
@@ -8,11 +8,16 @@ const props = defineProps({
   },
 })
 const style = computed(() => handleBackground(props.image))
-const components = defineComponent({ Default })
+const components = defineComponent({ Factor10Logo, })
 </script>
 
 <template>
-  <div class="slidev-layout w-full h-full" :style="style">
-    <slot />
+  <div class="bg-main w-full h-full p-0">
+    <factor10-logo></factor10-logo>
+    <div class="h-full w-full slidev-layout" :style="style">
+      <div class="myauto">
+        <slot />
+      </div>
+    </div>
   </div>
 </template>
